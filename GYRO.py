de68264d1aeeaafae2dda78f12370df42c1dcae2
The Young's <99293037+young1112@users.noreply.github.com>
2022-08-03 20:45:50 +0100

    Update GYRO.py

--- GYRO.py
+++ GYRO.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("GYRO").login_kamu()
+		__import__("GYRO").ANJIM()
 	else:
-		__import__("GYRO").login_kamu()
+		__import__("GYRO").ANJIM()
